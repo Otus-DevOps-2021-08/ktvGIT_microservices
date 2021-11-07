dbc89e1352843c8e4a8d45d79c71738ecc6e4b63
Terentev Kirill <terentevkv@ekassir.com>
2021-11-07 17:01:18 +0300

    final docker-3

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,5 +1,16 @@
 docker kill $(docker ps -q)
+
+
+
+# docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
+#  with volume
 docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
-docker run -d --network=reddit --network-alias=post ktv2020/post:1.6
+# docker run -d --network=reddit --network-alias=post ktv2020/post:1.6
 docker run -d --network=reddit --network-alias=comment ktv2020/comment:1.0
 docker run -d --network=reddit -p 9292:9292 ktv2020/ui:2.0
+
+# with env example
+docker run  -d  \
+-e POST_DATABASE_HOST='post_db' \
+-e POST_DATABASE='posts' \
+--network=reddit  --network-alias=post ktv2020/post:1.6
