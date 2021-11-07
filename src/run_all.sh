ea1ff32c51811b64340ea9d24ef014a9e36ea42c
Terentev Kirill <terentevkv@ekassir.com>
2021-11-07 06:15:32 +0300

    begin docker-3

---
+++ src/run_all.sh
@@ -0,0 +1,5 @@
+docker kill $(docker ps -q)
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
+docker run -d --network=reddit --network-alias=post ktv2020/post:1.6
+docker run -d --network=reddit --network-alias=comment ktv2020/comment:1.0
+docker run -d --network=reddit -p 9292:9292 ktv2020/ui:2.0
